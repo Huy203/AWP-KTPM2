5c81e4d32428bf831752932ade25fa36d1dca2e9
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-10-29 22:09:04 +0700

    update api

--- IA02/src/services/api.ts
+++ IA02/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.VITE_UNSPLASH_ENDPOINT || "http://localhost:3000";
-const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY || "";
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_KEY || "";
 
 const api = axios.create({
   baseURL: BASE_URL,
