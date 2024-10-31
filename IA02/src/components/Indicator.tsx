3f9cbcb267e977778276919baa0c207f071023bd
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-10-31 16:44:05 +0700

    fix: update vite template

--- IA02/src/components/Indicator.tsx
+++
@@ -1,7 +0,0 @@
-export const Indicator = () => {
-    return (
-      <div className="flex justify-center items-center my-4">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
-      </div>
-    );
-  };
