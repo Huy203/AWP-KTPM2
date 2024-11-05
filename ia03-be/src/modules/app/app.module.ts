b86e1efe06cdf7f117211d1ea80009903ef62a4a
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-11-05 20:42:46 +0700

    emit tsc

--- ia03-be/src/modules/app/app.module.ts
+++ ia03-be/src/modules/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from '@app/modules/auth/auth.module';
 import { PrismaService } from '@app/modules/prisma/prisma.service';
 import { PrismaModule } from '@app/modules/prisma/prisma.module';
 
